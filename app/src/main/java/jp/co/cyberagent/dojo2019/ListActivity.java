72e19129db9f774fd80673173c4b0e0f5e9444bd
nagata <nagata.software@gmail.com>
2019-08-08 18:05:53 +0900

    作業途中です． データベースに保存，データベースからの表示がよく分かりませんでした．

--- app/src/main/java/jp/co/cyberagent/dojo2019/ListActivity.java
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,18 +14,19 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 AppDatabase db = AppDatabaseSingleton.getInstance(ListActivity.this);
-                String a = db.userDao().toString();
-
+                final TextView textView = findViewById(R.id.textView);
+                final String a = db.userDao().toString();
 
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
                         // ui
-
+                        textView.setText(a);
                     }
                 });
             }
